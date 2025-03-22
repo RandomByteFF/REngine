7bb87acdf37c8f608feba70a62305bf3a99ff4c4
Bánáti Benedek <banatibenedek0@gmail.com>
2025-03-22 20:30:28 +0100

    Runner

---
+++ core/runner.cpp
@@ -0,0 +1,70 @@
+#include "runner.hpp"
+
+namespace REngine::Core {
+	void Runner::InitVulkan() {
+		Instance::Initialize(window);
+		device = Instance::GetInfo().device;
+		renderer.Create(window);
+		pipeline.SetLayout({
+			{vk::DescriptorType::eUniformBuffer, vk::ShaderStageFlagBits::eVertex},
+			{vk::DescriptorType::eCombinedImageSampler, vk::ShaderStageFlagBits::eFragment}
+		});
+		pipeline.Create("vert", "frag", renderer.GetSwapchain(), renderer.RenderPass());
+		textureImage.CreateImage(REngine::Loader::Image("test_files/viking_room.png"));
+		model.Load("test_files/viking_room.obj");
+		objects.push_back(Mesh());
+		objects[0].Create(pipeline, model.Verticies(), model.Indices());
+		objects[0].SetImage(textureImage, renderer.Sampler());
+		objects.push_back(Mesh());
+		objects[1].Create(pipeline, model.Verticies(), model.Indices());
+		objects[1].SetImage(textureImage, renderer.Sampler());
+		model.Destroy();
+	}
+
+	void Runner::MainLoop() {
+		Time::Start();
+		while(window.Update()) {
+			Time::Tick();
+			objects[0].Rotate(Time::Delta() * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+			objects[1].Rotate(Time::Delta() * glm::radians(10.0f), glm::vec3(0.0f, 1.0f, 1.0f));
+			renderer.Render(objects);
+		}
+
+		vkDeviceWaitIdle(device);
+	}
+
+	void Runner::Cleanup() {
+		renderer.Destroy();
+		textureImage.Destroy();
+
+		DescriptorPool::Cleanup();
+		for(auto i : objects) {
+			i.Destroy();
+		}
+		
+		REngine::Loader::Shader::Destroy();
+		
+		vkDestroyCommandPool(device, Instance::GetInfo().commandPool, nullptr);
+		pipeline.Destroy();
+		Instance::Destroy();
+		vkDestroyDevice(device, nullptr);
+		vkDestroySurfaceKHR(Instance::Get(), Instance::GetInfo().surface, nullptr);
+		vkDestroyInstance(Instance::Get(), nullptr);
+		window.Destroy();
+	}
+
+	int Runner::Run() {
+		try	{
+			window.CreateWindow();
+			InitVulkan();
+			MainLoop();
+			Cleanup();
+		}
+		catch(const std::exception& e) {
+			std::cerr << e.what() << '\n';
+			return EXIT_FAILURE;
+		}
+
+		return EXIT_SUCCESS;	
+	}
+}
