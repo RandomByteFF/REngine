9762047b31a4afc17b8d5abbbca4501fd120084f
Bánáti Benedek <banatibenedek0@gmail.com>
2025-04-19 22:57:56 +0200

    Camera rotation

--- core/camera.hpp
+++ core/camera.hpp
@@ -7,6 +7,8 @@ namespace REngine::Core {
 	class Camera {
 		Camera(const Camera &) = delete;
 		Camera &operator =(const Camera &) = delete;
+		glm::vec3 position;
+		glm::vec3 rotation;
 		glm::mat4 view;
 		glm::mat4 proj;
 		glm::mat4 vp;
@@ -15,8 +17,9 @@ namespace REngine::Core {
 		void resizedCallback(int width, int height);
 	public:
 		Camera();
-		Camera(glm::vec3 position, glm::vec3 lookAt);
-		void SetLook(glm::vec3 position, glm::vec3 lookAt);
+		Camera(glm::vec3 position);
+		void SetPosition(glm::vec3 position);
+		void Rotate(glm::vec3 rotation);
 		const glm::mat4 &VP();
 	};
 }
